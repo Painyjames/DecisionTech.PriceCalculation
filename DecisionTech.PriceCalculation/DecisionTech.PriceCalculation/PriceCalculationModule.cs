b7536e7b77cd6d61d2afabfcb0a537beb069798f
Victor Arbues <painyjames@gmail.com>
2016-08-07 17:42:15 +0100

    added DI module, removed Console project

---
+++ DecisionTech.PriceCalculation/DecisionTech.PriceCalculation/PriceCalculationModule.cs
@@ -0,0 +1,20 @@
+﻿namespace DecisionTech.PriceCalculation
+{
+	using Autofac;
+
+	public class PriceCalculationModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder
+				.RegisterType<Calculator>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+
+			builder
+				.RegisterType<DiscounterFactory>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+		}
+	}
+}
